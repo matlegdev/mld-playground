cec009e48a9b288ba10e27f17d2a31b75cf0aa5a
Mathieu Legras <mathieulegrasdev@gmail.com>
2021-08-16 13:38:56 +0200

    feat(warthog/shell): initialize ui library
    feat: rebase yarn

---
+++ libs/warthog/shell/ui/README.md
@@ -0,0 +1,7 @@
+# warthog-shell-ui
+
+Shell UI library developed for Warthog.
+
+## Running unit tests
+
+Run `nx test warthog-shell-ui` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/warthog/shell/ui/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'warthog-shell-ui',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/warthog/shell/ui',
+};
