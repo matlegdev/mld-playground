72e263b10c1dc9381a4605e4ef0d25e678e3ac9d
Mathieu Legras <mathieulegrasdev@gmail.com>
2021-08-24 22:05:12 +0200

    feat(warthog): add my top tracks

--- apps/warthog/src/app/app.tsx
+++ apps/warthog/src/app/app.tsx
@@ -1,8 +1,36 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, useTimeout } from '@chakra-ui/react';
 import { theme } from '../themes/dark-theme';
+import { SpotifyApiProvider, SpotifyAuthProvider, useSpotifyAuth } from '@mld/react-spotify-api';
+import { MyTopTracksPage } from '@mld/warthog/my-top-tracks/feature';
+import { Layout } from '@mld/warthog/shell/ui';
+import { FunctionComponent } from 'react';
+
+const ShowIfLoggedIn: FunctionComponent = ({ children }) => {
+  const { isSignedIn, signIn } = useSpotifyAuth();
+
+  // dirty hack so that we have some time to check whether the user is already authenticated
+  useTimeout(() => {
+    if (!isSignedIn) {
+      signIn();
+    }
+  }, 500);
+  return <div>{isSignedIn && children}</div>;
+};
 
 export function App() {
-  return <ChakraProvider theme={theme}>Warthog</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <SpotifyAuthProvider clientId={'554791a6a71b45e0a58a7105d15c6487'} scopes={['user-top-read']}>
+        <SpotifyApiProvider>
+          <ShowIfLoggedIn>
+            <Layout>
+              <MyTopTracksPage />
+            </Layout>
+          </ShowIfLoggedIn>
+        </SpotifyApiProvider>
+      </SpotifyAuthProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
