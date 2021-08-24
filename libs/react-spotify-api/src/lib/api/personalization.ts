d76cd86d52c93ee2cc2221b48546fa188b8cc33b
Mathieu Legras <mathieulegrasdev@gmail.com>
2021-08-24 22:01:30 +0200

    feat(react-spotify-api): add isValidating and config to useUserTopTracks

--- libs/react-spotify-api/src/lib/api/personalization.ts
+++ libs/react-spotify-api/src/lib/api/personalization.ts
@@ -2,6 +2,7 @@ import { useSpotifyApi } from '../SpotifyApiContext';
 import useSWR from 'swr';
 import { PagingObject } from '../models/pagingObject';
 import { TrackObject } from '../models/trackObject';
+import { SWRConfiguration } from 'swr/dist/types';
 
 export type UserTopTracksTimeRange = 'short_term' | 'medium_term' | 'long_term';
 
@@ -9,16 +10,18 @@ export interface UserTopTracksProps {
   time_range?: UserTopTracksTimeRange;
   limit?: number;
   offset?: number;
+  config?: SWRConfiguration<PagingObject<TrackObject>, unknown>;
 }
 
 export const useUserTopTracks = (props?: UserTopTracksProps) => {
   const { axios } = useSpotifyApi();
 
-  const { data, error } = useSWR<PagingObject<TrackObject>>(
+  const { data, error, isValidating } = useSWR<PagingObject<TrackObject>>(
     ['me/top/tracks', props?.time_range, props?.limit, props?.offset],
     (url: string, time_range: number, limit: number, offset: number) =>
-      axios.get(url, { params: { time_range, limit, offset } }).then((res) => res.data)
+      axios.get(url, { params: { time_range, limit, offset } }).then((res) => res.data),
+    props?.config
   );
 
-  return { data, error };
+  return { data, error, isValidating };
 };
